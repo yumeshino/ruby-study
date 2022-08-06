d4cec24d9bc4610f9e3fca2bc05d4ac0ce0f84a9
USERNAME <shinozakiyume88@gmail.com>
2022-08-06 09:47:33 +0900

    21. case文

--- sample.rb
+++ sample.rb
@@ -1,1 +1,48 @@
-puts 'Hel lo'
+value = 5
+
+symbol = case value
+            when 1
+              :one
+            when 2
+              :two
+            when 3
+              :three
+            when 4
+              :four
+            when 5
+              :five
+            else
+              :other
+          end
+
+puts symbol
+
+value = 11
+
+symbol = case value
+            when 1..5
+              :low
+            when 6..10
+              :high
+            else
+              :invalid
+          end
+
+puts symbol
+
+
+value = 'りんご'
+
+symbol = case value
+            when 'りんご','なし','キウイ','もも'
+              '果物'
+            when 'キャベツ','トマト','ねぎ'
+              '野菜'
+            else
+              '該当なし'
+          end
+
+puts symbol
+
+
+
