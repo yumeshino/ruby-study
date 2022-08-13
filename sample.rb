083f7cf7c121a92dcb5bc8639ad11ac6f85949ae
USERNAME <shinozakiyume88@gmail.com>
2022-08-14 08:42:28 +0900

    26. クラスにおけるメソッド定義

--- sample.rb
+++ sample.rb
@@ -104,6 +104,30 @@ Foo.first_method
 Foo.second_method
 Foo.third_method
 
+require 'date'
+
+class User
+  attr_accessor :name, :birthday
+
+  def initialize(name,birthday)
+    @name = name
+    @birthday = birthday
+  end
+
+  def display_birthday
+    @birthday.strtime('%Y年 %m月 %d日')
+  end
+
+  def innner_call_displya_birthday
+    self.display_birthday
+    display_birthday
+  end
+end
+
+birthday = Date.new(2000,1,1)
+user = User.new('sato',birthday)
+puts user.display_birthday
+puts user.inner_call_display_birthday
 
 
 
