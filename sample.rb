9ce6261562381b190aca197c3e4d0bf44a02ba3f
USERNAME <shinozakiyume88@gmail.com>
2022-08-13 21:58:58 +0900

    5. クラスメソッド、クラス変数

--- sample.rb
+++ sample.rb
@@ -84,5 +84,26 @@ book.name = 'はじめてのRuby'
 puts book.name
 
 
+class Foo
+  def self.first_method
+    puts 'first'
+  end
+
+  class << self
+    def second_method
+      puts 'second'
+    end
+
+    def third_method
+      puts 'third'
+    end
+  end
+end
+
+Foo.first_method
+Foo.second_method
+Foo.third_method
+
+
 
 
